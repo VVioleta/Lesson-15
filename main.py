b41ebda50006a85c00e8f1ee0b6a52d822c8b321
violetavester <violeta.vester@gmail.com>
2021-07-11 09:00:25 +0200

    new add

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_me():
+    return render_template("about.html")
+
+@app.route("/portfolio")
+def about_me():
+    return render_template("portfolio.html")
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True)
